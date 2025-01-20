9417a3c610ffdcced0035b05b6ed6379ee4f9803
Arup Dey <you@example.com>
2025-01-20 12:51:20 +0530

    add: configuration and host to npm

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts()],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'rte-ext',
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react', 'react-dom', 'remirror'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          remirror: 'Remirror',
+        },
+      }}
+  }
 })
